25c6e32bf204c9fd5bb7a880bb24ac0dc3be0dfa
kiana <kianakermani2001@gmail.com>
2023-08-07 14:46:07 +0330

    connect ViewModel To Page

--- TeacherStudentManagementSystem/TeacherStudentManagementSystem/Controllers/HomeController.cs
+++ TeacherStudentManagementSystem/TeacherStudentManagementSystem/Controllers/HomeController.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace TeacherStudentManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult Login()
         {
             return View();
         }
