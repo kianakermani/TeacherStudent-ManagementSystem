423586a4d01c68cc675fc3912e5138041730afb1
kiana <kianakermani2001@gmail.com>
2023-08-07 09:47:59 +0330

    first step for structure of login

---
+++ TeacherStudentManagementSystem/DataLayer/ViewModels/AccountViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.ViewModels
+{
+    public class loginViewModel
+    {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Please Fill {0}")]
+        public string Username { get; set; }
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please Fill {0}")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
--- TeacherStudentManagementSystem/TeacherStudentManagementSystem/Controllers/HomeController.cs
+++ TeacherStudentManagementSystem/TeacherStudentManagementSystem/Controllers/HomeController.cs
@@ -12,18 +12,8 @@ namespace TeacherStudentManagementSystem.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
