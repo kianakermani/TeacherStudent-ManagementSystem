22ef6458e31aca869d0d0d77bd8d351aef9e738c
kiana <kianakermani2001@gmail.com>
2023-08-24 17:23:43 +0330

    import first page of admin template and made log out button function

---
+++ TeacherStudentManagementSystem/TeacherStudentManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace TeacherStudentManagementSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminPanel()
+        {
+            return View();
+        }
+    }
+}
